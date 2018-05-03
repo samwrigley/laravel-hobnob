1e9a352cfff1e0f2e1900a42c7b7c87d012948b2
Sam Wrigley <sam@samwrigley.co.uk>
2018-05-03 22:04:58 +0100

    Created a `SocialNetwork` class responsible for returning the required social networks

---
+++ src/SocialNetwork.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace SamWrigley\Hobnob;
+
+use Illuminate\Support\Collection;
+
+class SocialNetwork
+{
+    /**
+     * Keys required by each social network.
+     *
+     * @var string[]
+     */
+    private $requiredKeys = [
+        'name',
+        'handle',
+        'url',
+    ];
+
+    /**
+     * Get social networks.
+     *
+     * @param  array|string[]  $networkNames
+     * @return \Illuminate\Support\Collection
+     */
+    public function getNetworks(array $networkNames = []): Collection
+    {
+        $networks = config('hobnob.networks');
+
+        return collect($networks)
+            ->rejectEmptyAssocValues()
+            ->filterByAssocKeys($this->requiredKeys)
+            ->filterByKeys($networkNames);
+    }
+
+    /**
+     * Get specific social networks.
+     *
+     * @param  string|string[]  $networkNames
+     * @return \Illuminate\Support\Collection
+     */
+    public static function get($networkNames): Collection
+    {
+        return (new static)->getNetworks(
+            is_array($networkNames) ? $networkNames : func_get_args()
+        );
+    }
+
+    /**
+     * Get all social networks.
+     *
+     * @return \Illuminate\Support\Collection
+     */
+    public static function all(): Collection
+    {
+        return (new static)->getNetworks();
+    }
+}
